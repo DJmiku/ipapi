dd99a42bfe6e8ae8d09fe6c1e92ff558b28d5a72
miku rules <mariocraft987dev+github1@gmail.com>
2024-09-05 14:12:59 -0700

    Delete database.js

--- database.js
+++
@@ -1,8 +0,0 @@
-const Database = require("easy-json-database");
-const db = new Database("./some-database.json", {
-    snapshots: {
-        enabled: true,
-        interval: 24 * 60 * 60 * 1000,
-        folder: './backups/'
-    }
-});
